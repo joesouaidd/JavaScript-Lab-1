74e2e60359d03718915bbbb440dd2a88368005f1
joe souaid <souaid.joe@hotmail.com>
2024-05-07 18:21:25 +0200

    lab2-done

--- Lab2/Scope/scope_lab.js
+++ Lab2/Scope/scope_lab.js
@@ -18,7 +18,7 @@ console.log(globalConst); // Output: "I'm a global constant"
 
 //Block Scope
 console.log(blockVar); //does not throw an error var has become global variabal
-console.log(blockLet);
+//console.log(blockLet); Throws ReferenceError
 
 function show(){
 var functionVar = "I'm a function-scoped var";
@@ -27,14 +27,28 @@ const functionConst = "I'm a function-scoped const";
 }
 show();
 
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+//console.log(functionVar); // Throws ReferenceError
+//console.log(functionLet); // Throws ReferenceError
+//console.log(functionConst); // Throws ReferenceError
 
 
 //conclusion:
 // var is a function scope var
 //var inside a block scope,outisde any function will become a global variable 
 //let and const is a block level scope
+
+
+{
+	let joe = "it's joe"
+	const constjoe = "it's const joe"
+	var varjoe = "it's var joe"
+	joe = "it's joee"
+	//constjoe = "it's const joee" //throws an error
+	varjoe = "it's var joee"
+
+}
+let joe = "it's joee"
+const constjoe = "it's const joee"
+var varjoe = "it's var joee"
 
 
